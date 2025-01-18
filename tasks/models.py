b052a662884f48b1264f384bde86d76b10afee23
Shamil Ahamed <shourovmd70@gmail.com>
2025-01-18 12:19:21 +0600

    module_7 done

--- tasks/models.py
+++ tasks/models.py
@@ -1,21 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
-    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1, related_name="projects")
-    assigned_to = models.ManyToManyField("Employee", related_name="tasks")
+    STATUS_CHOICES = [
+        ('PENDING', 'pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed')
+    ]
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1)
+    assigned_to = models.ManyToManyField(Employee, related_name="tasks")
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default="PENDING")
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-# One to One
-# Many to One
-# Many to Many
+    def __str__(self):
+        return self.title
 
 
 class TaskDetail(models.Model):
@@ -30,16 +40,17 @@ class TaskDetail(models.Model):
     task = models.OneToOneField(Task, on_delete=models.CASCADE, related_name="details")
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Details form Task {self.task.title}"
 
    
 class Project(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
     start_date = models.DateField()
- 
-
-class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.name
+ 
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
-from tasks.models import Employee, Task
+from tasks.models import Employee, Task, TaskDetail, Project
+from datetime import date
+from django.db.models import Q, Count, Max, Min, Avg
 
 # Create your views here.
 
@@ -35,21 +37,55 @@ def create_task(request):
             form.save()
             return render(request, 'task_form.html', {"form": form, "message":"Task added successfully"})
         
-
-            """For Django Form Data"""
-            # data = form.cleaned_data
-            # title = data.get('title')
-            # description = data.get('description')
-            # due_date = data.get('due_date')
-            # assigned_to = data.get('assigned_to') #list of employee id
-            # task = Task.objects.create(title=title, description=description, due_date=due_date)
-            # # Assign employee to task
-            # for emp_id in assigned_to:
-            #     employee = Employee.objects.get(id=emp_id)
-            #     task.assigned_to.add(employee)
-
-            # return HttpResponse("Task Added Successfully")
-        
     context = {"form": form}
     return render(request, "task_form.html", context)
+
+
+# def view_task(request):
+#     # retrieving all data from task model
+#     # tasks = Task.objects.all()
+
+#     # retrieve a specific task
+#     # task_3 = Task.objects.get(id=3)
+
+#     # Fetch the first task
+#     # first_task = Task.objects.first()
+#     # return render(request, "show_task.html", {"tasks": tasks, "task_3": task_3, "first_task": first_task})
+
+#     # show the task that are completed
+#     # task = Task.objects.filter(status="COMPLETED")
+#     # return render(request, "show_task.html", {"task": task})
+
+#     # show the task which due date is today
+#     # task = Task.objects.filter(due_date=date.today())
+#     # return render(request, "show_task.html", {"task": task})
+
+#     """Show the task whose priority is not low"""
+#     # task = TaskDetail.objects.exclude(priority="L")
+#     # return render(request, "show_task.html", {"task": task})
+
+#     """Show the task that contain any word and status pending"""
+#     # task = Task.objects.filter(title__icontains="c", status="PENDING")
+
+#     """Show the task which are pending or in progress"""
+#     task = Task.objects.filter(Q(status="PENDING") | Q(status="IN_PROGRESS"))
+#     return render(request, "show_task.html", {"task": task})
+
+# def view_task(request):
+#     # Select related queries (Foreign key, OneToOne)
+#     # task = Task.objects.select_related('details').all()
+#     # task = TaskDetail.objects.select_related('task').all()
+#     # task = Task.objects.select_related('project').all()
+
+#     """prefetch_related (reversse Foreign ke, manytomany)"""
+#     # task = Project.objects.prefetch_related('projects').all()
+#     task = Task.objects.prefetch_related('assigned_to').all()
+
+#     return render(request, "show_task.html", {"task": task})
+
+"""aggregations"""
+def view_task(request):
+    # task_count = Task.objects.aggregate(num_task=Count('id'))
+    projects = Project.objects.annotate(num_task=Count('task')).order_by('num_task')
+    return render(request, "show_task.html", {'projects': projects})
 
