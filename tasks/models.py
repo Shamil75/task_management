91b2529c1a07961219daab07cebcbe02a59d7e1b
Shamil Ahamed <shourovmd70@gmail.com>
2025-01-29 15:15:12 +0600

    Environmen created

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save, m2m_changed, post_delete
+from django.dispatch import receiver
+from django.core.mail import send_mail
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
@@ -54,3 +57,22 @@ class Project(models.Model):
     def __str__(self):
         return self.name
  
+@receiver(m2m_changed, sender= Task.assigned_to.through)
+def notify_employees_on_task_creation(sender, instance, action, **kwargs):
+    if action == 'post_add':
+        assigned_emails = [emp.email for emp in instance.assigned_to.all()]
+
+        send_mail(
+            "New Task Assigned",
+            f"You have been assigned to the task: {instance.title}",
+            "shourovmd70@gmail.com",
+            assigned_emails,
+            fail_silently=False,
+        )
+
+@receiver(post_delete, sender=Task)
+def delete_associate_details(sender, instance, **kwargs):
+    if instance.details:
+        instance.details.delete()
+
+        print('Deleted successfully')
